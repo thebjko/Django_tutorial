2e3c170e96b7a7c90ed3b8e27f3b48464225bf8b
thebjko <thebjko@gmail.com>
2021-06-04 20:40:08 +0900

    INSTALLED_APPS edited, migrations made.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', # python manage.py makemigrations polls
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
