27cde06a9855fcf3c7272f636023e1f0f0e2688a
thebjko <thebjko@gmail.com>
2021-06-08 14:57:16 +0900

    Views, URS's and Templates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
