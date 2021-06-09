94157b5aa962e8d5065604ab1bd1737dfb340f89
thebjko <thebjko@gmail.com>
2021-06-09 18:21:13 +0900

    Making Form

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = 'POLLS'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.index, name='INDEX'),
     # ex: /polls/5/
     path('<int:question_id>', views.detail, name='DETAIL'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='RESULTS'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='VOTE'),
 
 ]
