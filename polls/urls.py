a3d98c6d6047309e92cc1f1144f727e109025229
thebjko <thebjko@gmail.com>
2021-06-10 12:04:55 +0900

    Generic Display Views - DetailView, ListView

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,11 @@ from . import views
 app_name = 'POLLS'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='INDEX'),
+    path('', views.IndexView.as_view(), name='INDEX'),
     # ex: /polls/5/
-    path('<int:question_id>', views.detail, name='DETAIL'),
+    path('<int:pk>', views.DetailView.as_view(), name='DETAIL'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='RESULTS'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='RESULTS'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='VOTE'),
-
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'LATEST_QUESTION_LIST': latest_question_list
-    }
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'LATEST_QUESTION_LIST'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'QUESTION': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'QUESTION': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -32,7 +32,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-            'QUESTION': question,
+            'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
